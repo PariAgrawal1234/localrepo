e260f7e15915fde997703e7a70e79b6ac3040036
PariAgrawal1234 <pari.agrawal1314@gmail.com>
2025-06-21 21:54:24 +0530

    Kotlin new file

---
+++ new.kt
@@ -0,0 +1,6 @@
+fun int main(){
+    println("Hello, World!")
+    // This is a simple Kotlin program that prints "Hello, World!" to the console.
+    var name = readln()
+    println("Hello, $name!")
+}
